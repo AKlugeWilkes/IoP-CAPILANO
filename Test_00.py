03d715e10fbe1fd875bc4bcad9eb9b46e625b936
Balaji Gunaseelan <guna.balaji.abi@gmail.com>
2021-10-19 14:24:09 +0200

    Upload New File

---
+++ Test_00.py
@@ -0,0 +1,15 @@
+from Allocation_00 import Allocation
+
+holon_allocation = []
+#print(holon_allocation)
+query = [['Holon1'], ['Holon2'], ['Holon3']]
+
+holon_allocation = Allocation(query, holon_allocation)
+print(holon_allocation)
+holon_allocation = Allocation(query, holon_allocation)
+print(holon_allocation)
+holon_allocation = Allocation(query, holon_allocation)
+print(holon_allocation)
+holon_allocation = Allocation(query, holon_allocation)
+print(holon_allocation)
+
